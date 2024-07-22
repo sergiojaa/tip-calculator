e08bf1efb3ae377f1905201d7e81e3242fea16e1
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-07-22 23:19:51 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ const fivePercent = document.getElementById('five')
 const numberOfPeople = document.getElementById('people-count')
 const tipAmount = document.getElementById('tip-amount')
 const totalAmount = document.getElementById('total-amount')
+
+let bilValue;
+let percentValue;
+let peopleValue
+billAmount.addEventListener('input', (event) =>{
+    bilValue = event.target.value
+    calculation()
+})
+
+fivePercent.addEventListener('click', (event)=>{
+    percentValue = parseInt(event.target.value) 
+    calculation()
+})
+
+numberOfPeople.addEventListener('input', (event)=>{
+    peopleValue = parseInt(event.target.value) 
+    calculation()
+})
+
+function calculation(){
+    const tipResultValue = bilValue * (percentValue / 100)/peopleValue
+    const totalAmountValue = tipResultValue + bilValue / peopleValue
+    console.log(totalAmountValue)
+}
