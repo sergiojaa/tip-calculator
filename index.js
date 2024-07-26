f39ea3b7b77ae6a8f17c20b45333c9f26f32cccb
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-07-26 20:30:11 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const tipButtons = [...document.getElementsByClassName('tip-buttons')];
 const numberOfPeople = document.getElementById('people-count')
 const tipAmount = document.getElementById('tip-amount')
 const totalAmount = document.getElementById('total-amount')
-const reset = document.getElementById('reset')
-let bilValue = 0;
-let percentValue = 0;
-let peopleValue = 0;
+// const reset = document.getElementById('reset')
+let bilValue=0
+let percentValue =0
+let peopleValue =0
 
 billInput.addEventListener('input', (event)=>{
-    bilValue = event.target.value
+    bilValue =  parseInt(event.target.value) 
     calculation()
 })
 
@@ -20,26 +20,22 @@ tipButtons.forEach(button => {
         calculation()
     });
 });
-customTipInput.addEventListener('input', (event) => {
-    percentValue = parseFloat(event.target.value);
-    calculation();
-});
+
 numberOfPeople.addEventListener('input', (event)=>{
-    peopleValue = event.target.value
+    peopleValue = parseInt(event.target.value) 
     calculation()
 })
 
 
     function calculation(){
-        const tipResultValue = (bilValue * (percentValue / 100)) / peopleValue
-        const totalResultValue = tipResultValue + bilValue / peopleValue
+             const tipResultValue = (bilValue * (percentValue / 100)) / peopleValue
+            const  totalResultValue = tipResultValue + bilValue / peopleValue
 
-        if(peopleValue){
-             tipAmount.textContent = `$${tipResultValue}`
-             totalAmount.textContent = `$${totalResultValue }`
-
-        }
         
-    }
+        if(peopleValue){
+           
+            tipAmount.textContent = `$${tipResultValue.toFixed(2)}`
+            totalAmount.textContent = `$${totalResultValue.toFixed(2) }`
 
- 
+       }  
+    }
