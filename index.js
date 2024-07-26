8e892f3d83ed710481c70e9944cf79dcd759c1f9
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-07-26 17:54:53 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-const billAmount = document.getElementById('bill-amount')
-const fivePercent = document.getElementById('five')
+const billInput = document.getElementById('bill-amount')
+const tipButtons = [...document.getElementsByClassName('tip-buttons')];
 const numberOfPeople = document.getElementById('people-count')
 const tipAmount = document.getElementById('tip-amount')
 const totalAmount = document.getElementById('total-amount')
+const reset = document.getElementById('reset')
+let bilValue = 0;
+let percentValue = 0;
+let peopleValue = 0;
 
-let bilValue;
-let percentValue;
-let peopleValue
-billAmount.addEventListener('input', (event) =>{
+billInput.addEventListener('input', (event)=>{
     bilValue = event.target.value
     calculation()
 })
 
-fivePercent.addEventListener('click', (event)=>{
-    percentValue = parseInt(event.target.value) 
-    calculation()
-})
 
+tipButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        percentValue = parseInt(event.target.textContent)
+        calculation()
+    });
+});
+customTipInput.addEventListener('input', (event) => {
+    percentValue = parseFloat(event.target.value);
+    calculation();
+});
 numberOfPeople.addEventListener('input', (event)=>{
-    peopleValue = parseInt(event.target.value) 
+    peopleValue = event.target.value
     calculation()
 })
 
-function calculation(){
-    const tipResultValue = bilValue * (percentValue / 100)/peopleValue
-    const totalAmountValue = tipResultValue + bilValue / peopleValue
-    console.log(totalAmountValue)
-}
+
+    function calculation(){
+        const tipResultValue = (bilValue * (percentValue / 100)) / peopleValue
+        const totalResultValue = tipResultValue + bilValue / peopleValue
+
+        if(peopleValue){
+             tipAmount.textContent = `$${tipResultValue}`
+             totalAmount.textContent = `$${totalResultValue }`
+
+        }
+        
+    }
+
+ 
